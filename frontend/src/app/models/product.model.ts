fc1aa8356cd42caa65078dc52d9e01642d03d9e7
Akhilan A <78470490+Akhilan11@users.noreply.github.com>
2024-10-01 20:55:12 +0530

    Add files via upload

--- frontend/src/app/models/product.model.ts
+++ frontend/src/app/models/product.model.ts
@@ -7,3 +7,11 @@ export interface Product {
     category: string;
 }
   
+// Define a separate interface for new products without _id
+export interface NewProduct {
+    imageUrl: string;
+    name: string;
+    description: string;
+    price: number;
+    category: string;
+}
